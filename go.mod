8c3169fb2cbf416bafdd03f0beab40bd2c3df53c
kakie <kakie518@gmail.com>
2022-12-19 11:06:44 +0800

    dst & cobra

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module d2
 
 go 1.18
 
-require golang.org/x/exp v0.0.0-20221217163422-3c43f8badb15
+require (
+	github.com/spf13/cobra v1.6.1
+	golang.org/x/exp v0.0.0-20221217163422-3c43f8badb15
+)
+
+require (
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+)
--- main.go
+++ main.go
@@ -8,41 +8,110 @@ import (
 	"os"
 	"strconv"
 
-	"golang.org/x/exp/constraints"
+	"github.com/spf13/cobra"
 )
 
 var (
-	srcFile = "../src.out"
-	dstFile = "../dst.out"
+	typ string
+
+	root cobra.Command
+	args []argument
 )
 
-type number interface {
-	constraints.Signed | constraints.Float
+func init() {
+	var revs []bool
+	var paths, seps []string
+
+	root = cobra.Command{
+		Use:   "",
+		Short: "D2",
+		Long:  "Compute delta between columns of 2 files of same class",
+		Run: func(_ *cobra.Command, _ []string) {
+			switch typ {
+			case "i", "f":
+			default:
+				fmt.Fprintf(os.Stderr, "type: %s\n", typ)
+				fmt.Fprintln(os.Stderr, "dst type specify: i(int) or f(float)")
+				os.Exit(-1)
+			}
+
+			if len(paths) != 2 || len(seps) != 2 || len(revs) != 2 {
+				fmt.Fprintf(os.Stderr, "paths: %+v\n", paths)
+				fmt.Fprintf(os.Stderr, "seps: %+v\n", seps)
+				fmt.Fprintf(os.Stderr, "revs: %+v\n", revs)
+				fmt.Fprintln(os.Stderr, "there should be 3 pairs of params(path, sep, rev), each pair has two values.")
+				os.Exit(-1)
+			}
+
+			for i := 0; i < 2; i++ {
+				if fi, err := os.Stat(paths[i]); err == nil {
+					if fi.IsDir() {
+						fmt.Fprintf(os.Stderr, "the path should be a file instead of a directory, path: %s.\n", paths[i])
+						os.Exit(-1)
+					}
+				} else if os.IsNotExist(err) {
+					fmt.Fprintf(os.Stderr, "the path file not exists, path: %s.\n", paths[i])
+					os.Exit(-1)
+				} else {
+					fmt.Fprintf(os.Stderr, "stats error, path: %s, err: %+v\n", paths[i], err)
+					os.Exit(-1)
+				}
+
+				args = append(args, argument{
+					paths[i],
+					seps[i],
+					revs[i],
+				})
+			}
+
+			main()
+		},
+	}
+
+	root.Flags().StringVarP(&typ, "type", "t", "i", "dst type specify: i(int) or f(float)")
+
+	root.Flags().StringSliceVarP(&paths, "path", "p", []string{}, "dst file path")
+	root.Flags().StringSliceVarP(&seps, "sep", "s", []string{" ", " "}, "dst separator")
+	root.Flags().BoolSliceVarP(&revs, "rev", "r", []bool{false, false}, "dst column sequence: false(forward) or true(reverse)")
+
+	root.Execute()
+
 }
 
-type cache[T number] map[string]T
+type argument struct {
+	path string
+	sep  string
+	rev  bool
+}
 
-type assignFunc[T number] func([2][]byte, cache[T])
+type number interface {
+	int64 | float64
+}
 
-func main() {
-	srcM := make(map[string]int64)
-	{
-		err := read(srcFile, srcM, []byte("\t"), retain[int64])
-		fmt.Printf("%+v\n", len(srcM))
-		fmt.Printf("err: %+v\n", err)
-	}
+// type numbers[N number] []N
 
-	dstM := make(map[string]int64)
-	{
-		err := read(dstFile, dstM, []byte(" "), exchange[int64])
-		fmt.Printf("%+v\n", len(dstM))
-		fmt.Printf("err: %+v\n", err)
-	}
+type cache[N number] map[string]N
 
-	for sk, sv := range srcM {
-		dv, ok := dstM[sk]
+type assignFunc[N number] func([2][]byte, cache[N])
+
+type dst[N number] struct {
+	path string
+	sep  []byte
+	c    cache[N]
+	fn   assignFunc[N]
+}
+
+type dsts[N number] []dst[N]
+
+func (ds dsts[N]) set(v dst[N]) {
+	ds = append(ds, v)
+}
+
+func (ds dsts[N]) compute() {
+	for sk, sv := range ds[0].c {
+		dv, ok := ds[1].c[sk]
 		if !ok {
-			fmt.Printf("only src: %d, k: %s\n", sv, sk)
+			fmt.Printf("only in %s: %d, k: %s\n", ds[0].path, sv, sk)
 			continue
 		}
 
@@ -53,12 +122,52 @@ func main() {
 		}
 	}
 
-	for dk, dv := range dstM {
-		_, ok := srcM[dk]
+	for k, v := range ds[1].c {
+		_, ok := ds[0].c[k]
 		if !ok {
-			fmt.Printf("only dst: %d, k: %s\n", dv, dk)
+			fmt.Printf("only in %s, k: %s, v: %+v\n", ds[1].path, k, v)
 			continue
 		}
+	}
+}
+
+func main() {
+	if typ == "f" {
+		var ds dsts[float64]
+
+		for _, a := range args {
+			d := dst[float64]{
+				a.path,
+				[]byte(a.sep),
+				make(map[string]float64),
+				exchange[float64],
+			}
+			err := d.read()
+			fmt.Printf("%+v\n", len(d.c))
+			fmt.Printf("err: %+v\n", err)
+
+			ds = append(ds, d)
+		}
+
+		ds.compute()
+	} else {
+		var ds dsts[int64]
+
+		for _, a := range args {
+			d := dst[int64]{
+				a.path,
+				[]byte(a.sep),
+				make(map[string]int64),
+				retain[int64],
+			}
+			err := d.read()
+			fmt.Printf("%+v\n", len(d.c))
+			fmt.Printf("err: %+v\n", err)
+
+			ds = append(ds, d)
+		}
+
+		ds.compute()
 	}
 }
 
@@ -78,16 +187,16 @@ func exchange[N number](bs [2][]byte, m cache[N]) {
 	m[string(bs[1])] = N(i)
 }
 
-func extract(b []byte, delimiter []byte) [2][]byte {
-	bs := bytes.Split(b, delimiter)
+func extract(b []byte, sep []byte) [2][]byte {
+	bs := bytes.Split(b, sep)
 	if len(bs) != 2 {
 		panic(len(bs))
 	}
 	return [2][]byte{bs[0], bs[1]}
 }
 
-func read[N number](path string, m cache[N], delimiter []byte, fn assignFunc[N]) (err error) {
-	f, err := os.Open(path)
+func (d dst[_]) read() (err error) {
+	f, err := os.Open(d.path)
 	if err != nil {
 		return
 	}
@@ -104,9 +213,9 @@ func read[N number](path string, m cache[N], delimiter []byte, fn assignFunc[N])
 			return err
 		}
 
-		b2 := extract(b, delimiter)
+		b2 := extract(b, d.sep)
 		// fmt.Printf("b2: %s\n", b2)
-		fn(b2, m)
+		d.fn(b2, d.c)
 	}
 
 	return
